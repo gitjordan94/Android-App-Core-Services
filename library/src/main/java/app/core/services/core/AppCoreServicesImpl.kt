b1ce9cc68f5a262d3de03a02d13c682fc3b8e259
gitjordan94 <jordancodes94@gmail.com>
2026-01-06 13:28:23 +0400

    Added storeCountry to configuration result

--- library/src/main/java/app/core/services/core/AppCoreServicesImpl.kt
+++ library/src/main/java/app/core/services/core/AppCoreServicesImpl.kt
@@ -205,6 +205,7 @@ internal class AppCoreServicesImpl(
                     activePaywall = activePaywallName,
                     attribution = attribution,
                     purchases = purchases,
+                    storeCountry = storeCountry,
                     isFirstLaunch = isFirstAppLaunch
                 )
 
